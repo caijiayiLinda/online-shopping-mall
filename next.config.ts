c5a01feca0d421f18dd1f204e98f038c05dfe73f
caijy <caijy@shdata.com>
2025-02-13 19:48:49 +0800

    Hide the versions of OS, Web Server and Node in HTTP response headers

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Powered-By',
+            value: '',
+          },
+          {
+            key: 'Server',
+            value: '',
+          },
+        ],
+      },
+    ];
+  }
 };
 
 export default nextConfig;
